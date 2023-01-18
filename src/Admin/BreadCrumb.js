a8bf69aa92c1ff64f9d7f79e0aeed13e77a3787b
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-01-18 12:03:47 +0530

    ok

--- src/Admin/BreadCrumb.js
+++ src/Admin/BreadCrumb.js
@@ -1,29 +1,12 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import properties from "./data.json";
 
+let savedUserType = "";
 const BreadCrumb = ({ userType, emailOfCurrentUser }) => {
-  const [activeInnerText, setActiveInnerText] = useState("");
-  const [dataToShow, setDataToShow] = useState({
-    userName: "",
-    property: "",
-  });
-
-  const { property } = dataToShow;
+  const [isUserPageActive, setIsUserPageActive] = useState(false);
   const testFn = () => {
-    const activeLinks = document.querySelectorAll(".nav-link.active");
-    const propertyIdFromUrl = window.location.href.slice(-5);
-    setActiveInnerText(activeLinks[0].innerText);
-
-    properties.forEach((property) => {
-      if (property._id === propertyIdFromUrl) {
-        setDataToShow({
-          ...dataToShow,
-          property: property.category + " - " + property.city_name,
-        });
-      }
-    });
+    setIsUserPageActive(window.location.href.includes("/admin/users"));
   };
 
   useEffect(() => {
@@ -40,32 +23,27 @@ const BreadCrumb = ({ userType, emailOfCurrentUser }) => {
               Dashboard
             </NavLink>
           </li>
-
-          {activeInnerText === "Users" ? (
+          {isUserPageActive ? (
             <>
               <NavLink to="/admin/users" className="breadcrumb-item">
                 Users
               </NavLink>
-              <li className="breadcrumb-item text-secondary">{userType}</li>
-            </>
-          ) : activeInnerText === "Properties" ? (
-            <>
-              <NavLink to="/admin/property" className="breadcrumb-item">
-                Properties
-              </NavLink>
-              {property ? (
-                <li className="breadcrumb-item text-secondary">{property}</li>
+              {userType ? (
+                <li className="breadcrumb-item text-secondary">
+                  {(savedUserType = userType)}
+                </li>
               ) : (
-                ""
+                <></>
               )}
             </>
           ) : (
-            <>
-              <li className="breadcrumb-item text-secondary">
-                {emailOfCurrentUser}
-              </li>
-            </>
+            <></>
           )}
+
+          {/* <NavLink to="/admin/property" className="breadcrumb-item">
+            Properties
+          </NavLink> */}
+          {/* <li className="breadcrumb-item text-secondary">{property}</li> */}
         </ol>
       </nav>
     </>
