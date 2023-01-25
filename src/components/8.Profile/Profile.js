656ffb0753d10bef28c398395b85b1a5e8a68324
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-01-25 15:09:07 +0530

    ok

--- src/components/8.Profile/Profile.js
+++ src/components/8.Profile/Profile.js
@@ -1,7 +1,123 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../1.CommonLayout/Layout";
+import axios from "axios";
 
 const Profile = () => {
+  const data = JSON.parse(localStorage.getItem("data"));
+  const [userType, setUserType] = useState("");
+  // To store updated user details.
+  const [commonUserDetails, setCommonUserDetails] = useState({});
+
+  // Object destructuring.
+  const {
+    user_type,
+    mobile_number,
+    locality,
+    address,
+    city,
+    state_name,
+    zip,
+    email,
+  } = commonUserDetails;
+
+  const [orgUserDetails, setOrgUserDetails] = useState({});
+
+  // Object destructuring.
+  const {
+    cin_number,
+    company_name,
+    gst_number,
+    organization_type,
+    tan_number,
+  } = orgUserDetails;
+
+  const [individualUserDetails, setIndividualUserDetails] = useState({});
+
+  // Object destructuring.
+  const { first_name, middle_name, last_name, pan_number, aadhar_number } =
+    individualUserDetails;
+
+  // Function will provide login token of user from localStorage and also some urls are stored in this function.
+  const setHeaderAndUrl = () => {
+    let headers = "";
+    if (data) {
+      headers = { Authorization: data.logintoken };
+    }
+    let url = `/sam/v1/user-registration/auth`;
+    return [headers, url];
+  };
+
+  // Function will get the data of user whose details are to be edited.
+  const getUserProfileDetails = async () => {
+    const [headers, url] = setHeaderAndUrl();
+    if (data) {
+      const userId = data.userId;
+      await axios
+        .get(`${url}/${userId}`, { headers: headers })
+        .then(async (res) => {
+          const { individual_user, org_user, user_details } = res.data;
+          if (individual_user) {
+            const {
+              first_name,
+              middle_name,
+              last_name,
+              pan_number,
+              aadhar_number,
+            } = individual_user;
+            setIndividualUserDetails({
+              first_name: first_name,
+              middle_name: middle_name,
+              last_name: last_name,
+              pan_number: pan_number,
+              aadhar_number: aadhar_number,
+            });
+          } else if (org_user) {
+            const {
+              cin_number,
+              company_name,
+              gst_number,
+              organization_type,
+              tan_number,
+            } = org_user;
+            setOrgUserDetails({
+              cin_number: cin_number,
+              company_name: company_name,
+              gst_number: gst_number,
+              organization_type: organization_type,
+              tan_number: tan_number,
+            });
+          }
+          const {
+            user_type,
+            mobile_number,
+            locality,
+            city,
+            state_name,
+            state_id,
+            zip,
+            email_address,
+            address,
+          } = user_details;
+          setUserType(user_type);
+          setCommonUserDetails({
+            state_id: parseInt(state_id),
+            address: address,
+            mobile_number: mobile_number,
+            locality: locality,
+            city: city,
+            state_name: state_name,
+            zip: zip,
+            email: email_address,
+            user_type: user_type,
+          });
+        });
+    }
+  };
+
+  useEffect(() => {
+    getUserProfileDetails();
+  }, []);
+
   return (
     <Layout>
       <section className="profile-wrapper section-padding min-100vh">
@@ -22,11 +138,15 @@ const Profile = () => {
                   <div className="row">
                     <div className="col-xl-12 text-center text-md-start">
                       <span className="fw-bold fs-3 text-warning">
-                        John Butler
+                        {userType === "Individual User"
+                          ? `${first_name} ${last_name}`
+                          : company_name}
                       </span>
                       <br />
                       <span className="text-muted">
-                        Xyz Designation of user
+                        {userType === "Individual User"
+                          ? `${user_type}`
+                          : `${userType} (${organization_type})`}
                       </span>
                     </div>
                   </div>
@@ -41,18 +161,18 @@ const Profile = () => {
                     </div>
                     <div className="offset-md-1 col-md-3 mt-4 mt-md-0">
                       <div>
-                        <p className="text-muted">Some Heading</p>
-                        <p>Lorem ipsum dolor sit.</p>
+                        <p className="text-muted">Mobile</p>
+                        <p>{mobile_number}</p>
                       </div>
                       <div className="mt-4 mt-md-5">
-                        <p className="text-muted">Some Heading</p>
-                        <p>Lorem ipsum dolor sit.</p>
+                        <p className="text-muted">Email</p>
+                        <p>{email}</p>
                       </div>
                     </div>
                     <div className="col-md-3">
                       <div className="mt-4 mt-md-0">
-                        <p className="text-muted">Some Heading</p>
-                        <p>Lorem ipsum dolor sit.</p>
+                        <p className="text-muted">City & State</p>
+                        <p>{`${city}, ${state_name}`}</p>
                       </div>
                       <div className="mt-4 mt-md-5">
                         <p className="text-muted">Some Heading</p>
