b26ae2f95c41d326fd4b42603342378f31d8dd7a
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-02-10 11:07:53 +0530

    ok

--- src/Admin/Property/UploadProperties.js
+++ src/Admin/Property/UploadProperties.js
@@ -133,11 +133,15 @@ const UploadProperties = () => {
     };
     const chunks = Math.ceil(file.size / chunkSize) - 1;
     const isLastChunk = currentChunkIndex === chunks;
-    await axios.post(url, dataToPost, { headers: headers }).then(() => {
+    await axios.post(url, dataToPost, { headers: headers }).then((res) => {
       if (isLastChunk) {
-        setUniqueUploadId(uuid());
-        toast.success("Data saved successfully");
-        onCancelClick();
+        if (res.data === "File uploaded successfully") {
+          setUniqueUploadId(uuid());
+          toast.success("Data saved successfully");
+          onCancelClick();
+        } else {
+          toast.error("Some error occurred");
+        }
       }
     });
     // console.warn("IS LAST CHUNK: ", isLastChunk);
