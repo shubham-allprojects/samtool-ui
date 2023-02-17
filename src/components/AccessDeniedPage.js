8badb80a21a135ffb4d753c72fb228f35c14e375
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-02-17 12:11:36 +0530

    ok

--- src/components/AccessDeniedPage.js
+++ src/components/AccessDeniedPage.js
@@ -12,18 +12,18 @@ const AccessDeniedPage = () => {
     <section className="access-denied-wrapper">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-xl-7 text-center">
+          <div className="col-xl-7 col-lg-6 col-md-8 text-center">
             <h1 className="fw-bold text-danger">
-              <i className="bi bi-slash-circle me-1"></i> Access Denied 
+              <i className="bi bi-slash-circle me-1"></i> Access Denied
             </h1>
             <span className="text-secondary">
               You currently does not have access to this page.
             </span>
-            <div>
-              <NavLink to="/" className="btn btn-sm btn-outline-secondary mt-md-3 mt-2 me-2">
-               <i className="bi bi-arrow-left pe-1"></i> Back to home 
+            <div className="mt-md-3 mt-2">
+              <NavLink to="/" className="btn btn-sm btn-outline-secondary me-2">
+                <i className="bi bi-arrow-left pe-1"></i> Back to home
               </NavLink>
-              <NavLink to="/login" className="btn btn-sm btn-outline-secondary mt-md-3 mt-2">
+              <NavLink to="/login" className="btn btn-sm btn-outline-secondary">
                 Back to Login <i className="bi bi-arrow-right ps-1"></i>
               </NavLink>
             </div>
