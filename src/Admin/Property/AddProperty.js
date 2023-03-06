9efdb9f704d4fc1515ec0a76c37226056ad0d3dd
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-03-06 16:39:22 +0530

    ok

--- src/Admin/Property/AddProperty.js
+++ src/Admin/Property/AddProperty.js
@@ -122,32 +122,142 @@ const AddProperty = () => {
                           <div className="col-xl-4">
                             <div className="form-group">
                               <label
-                                htmlFor="property_number"
+                                htmlFor="market_price"
                                 className="form-label common-btn-font"
                               >
-                                Property Number
+                                Market price (Rs.)
                               </label>
                               <input
-                                type="text"
-                                id="property_number"
-                                name="property_number"
                                 className="form-control"
+                                type="number"
+                                id="market_price"
+                                name="market_price"
                               />
                             </div>
                           </div>
                           <div className="col-xl-4">
-                            <label
-                              htmlFor="market_price"
-                              className="form-label common-btn-font"
-                            >
-                              Market price (Rs.)
-                            </label>
-                            <input
-                              className="form-control"
-                              type="number"
-                              id="market_price"
-                              name="market_price"
-                            />
+                            <div className="form-group">
+                              <label
+                                htmlFor="ready_reckoner_price"
+                                className="form-label common-btn-font"
+                              >
+                                Ready reckoner price (Rs.)
+                              </label>
+                              <input
+                                type="number"
+                                id="ready_reckoner_price"
+                                name="ready_reckoner_price"
+                                className="form-control"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        {/* Row 3 */}
+                        <div className="row mb-3">
+                          <div className="col-xl-4">
+                            <div className="form-group">
+                              <label
+                                className="form-label common-btn-font"
+                                htmlFor="expected_price"
+                              >
+                                Expected price (Rs.)
+                              </label>
+                              <input
+                                type="number"
+                                className="form-control"
+                                id="expected_price"
+                                name="expected_price"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-xl-4">
+                            <div className="form-group">
+                              <label
+                                htmlFor="completion_date"
+                                className="form-label common-btn-font"
+                              >
+                                Completion date
+                              </label>
+                              <input
+                                className="form-control"
+                                type="date"
+                                id="completion_date"
+                                name="completion_date"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-xl-4">
+                            <div className="form-group">
+                              <label
+                                htmlFor="purchase_date"
+                                className="form-label common-btn-font"
+                              >
+                                Purchase date
+                              </label>
+                              <input
+                                className="form-control"
+                                type="date"
+                                id="purchase_date"
+                                name="purchase_date"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        {/* Row 4 */}
+                        <div className="row mb-3">
+                          <div className="col-xl-4">
+                            <div className="form-group">
+                              <label
+                                htmlFor="mortgage_date"
+                                className="form-label common-btn-font"
+                              >
+                                Mortgage date
+                              </label>
+                              <input
+                                className="form-control"
+                                type="date"
+                                id="mortgage_date"
+                                name="mortgage_date"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-xl-4">
+                            <div className="form-group">
+                              <label
+                                className="form-label common-btn-font"
+                                htmlFor="is_sold"
+                              >
+                                Is sold?
+                              </label>
+                              <select
+                                id="is_sold"
+                                name="is_sold"
+                                className="form-select"
+                              >
+                                <option value="0">No</option>
+                                <option value="1">Yes</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="col-xl-4">
+                            <div className="form-group">
+                              <label
+                                className="form-label common-btn-font"
+                                htmlFor="is_available_for_sale"
+                              >
+                                Available for sale?
+                              </label>
+                              <select
+                                id="is_available_for_sale"
+                                name="is_available_for_sale"
+                                className="form-select"
+                              >
+                                <option value="0">No</option>
+                                <option value="1">Yes</option>
+                              </select>
+                            </div>
                           </div>
                         </div>
                       </div>
