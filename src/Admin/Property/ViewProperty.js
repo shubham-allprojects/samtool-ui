0ab436f826a41ccf0fde982a94a1c35f170dee0b
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-03-17 11:11:36 +0530

    ok

--- src/Admin/Property/ViewProperty.js
+++ src/Admin/Property/ViewProperty.js
@@ -66,13 +66,87 @@ const ViewProperty = () => {
               <h3 className="fw-bold text-primary pb-2">{type_name}</h3>
               <div className="container-fluid border border-1 p-3">
                 <div className="row">
+                  <div className="col-12"></div>
                   <div className="col-xl-5">
-                    <div className="currentPropertyImgWrapper">
-                      <img
-                        src="/images2.jpg"
-                        alt="property-pic"
-                        className="img-fluid"
-                      />
+                    {/* <img
+                      src="/images2.jpg"
+                      alt="property-pic"
+                      className="img-fluid"
+                    /> */}
+                    <div
+                      id="carouselExampleIndicators"
+                      className="carousel slide"
+                      data-bs-ride="carousel"
+                    >
+                      <div className="carousel-indicators">
+                        <button
+                          type="button"
+                          data-bs-target="#carouselExampleIndicators"
+                          data-bs-slide-to="0"
+                          className="active"
+                          aria-current="true"
+                          aria-label="Slide 1"
+                        ></button>
+                        <button
+                          type="button"
+                          data-bs-target="#carouselExampleIndicators"
+                          data-bs-slide-to="1"
+                          aria-label="Slide 2"
+                        ></button>
+                        <button
+                          type="button"
+                          data-bs-target="#carouselExampleIndicators"
+                          data-bs-slide-to="2"
+                          aria-label="Slide 3"
+                        ></button>
+                      </div>
+                      <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="2000">
+                          <img
+                            src="/images2.jpg"
+                            className="d-block w-100"
+                            alt="..."
+                          />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                          <img
+                            src="/images2.jpg"
+                            className="d-block w-100"
+                            alt="..."
+                          />
+                        </div>
+                        <div className="carousel-item">
+                          <img
+                            src="/images2.jpg"
+                            className="d-block w-100"
+                            alt="..."
+                          />
+                        </div>
+                      </div>
+                      <button
+                        className="carousel-control-prev"
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="prev"
+                      >
+                        <span
+                          className="carousel-control-prev-icon"
+                          aria-hidden="true"
+                        ></span>
+                        <span className="visually-hidden">Previous</span>
+                      </button>
+                      <button
+                        className="carousel-control-next"
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="next"
+                      >
+                        <span
+                          className="carousel-control-next-icon"
+                          aria-hidden="true"
+                        ></span>
+                        <span className="visually-hidden">Next</span>
+                      </button>
                     </div>
                   </div>
                   {/* <div className="col-xl-5 d-flex justify-content-center align-items-center">
