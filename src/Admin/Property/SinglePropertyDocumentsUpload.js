0ae43cb59d12cf04e93bdd029fe0020cfffbef50
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-03-20 16:45:52 +0530

    ok

--- src/Admin/Property/SinglePropertyDocumentsUpload.js
+++ src/Admin/Property/SinglePropertyDocumentsUpload.js
@@ -4,8 +4,10 @@ import Layout from "../../components/1.CommonLayout/Layout";
 import AdminSideBar from "../AdminSideBar";
 import BreadCrumb from "../BreadCrumb";
 import { v4 as uuid } from "uuid";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const chunkSize = 100000;
+const chunkSize = 1000 * 1024;
 let authHeader = "";
 const SinglePropertyDocumentsUpload = () => {
   const data = JSON.parse(localStorage.getItem("data"));
@@ -41,29 +43,23 @@ const SinglePropertyDocumentsUpload = () => {
     reader.readAsDataURL(blob);
   };
 
-  const uploadImageChunk = (readerEvent) => {
+  const uploadImageChunk = async (readerEvent) => {
     let fileSize = 0;
     const file = imageFiles[currentImageFileIndex];
     const size = Math.round(file.size / 1024) + 1;
     fileSize = size >= 1024 ? (size / 1024).toFixed(1) + " MB" : size + " KB";
     const data = readerEvent.target.result.split(",")[1];
-    const detailsToShow = `Name: ${
-      file.name
-    } ----- Size: ${fileSize} ----- TotalChunks: ${Math.ceil(
-      file.size / chunkSize
-    )} ----- Data For Chunk: ${currentChunkIndexOfImage + 1} is ====> ${data}`;
-
     const detailsToPost = {
       upload_id: uniqueId,
       chunk_number: `${currentChunkIndexOfImage + 1}`,
       total_chunks: `${Math.ceil(file.size / chunkSize)}`,
       total_file_size: `${fileSize}`,
       file_name: `${file.name}`,
-      property_id: 1,
-      data: uuid(),
+      property_id: 2,
+      data: `${data}`,
     };
 
-    console.log(detailsToPost);
+    // console.log(detailsToPost);
 
     // const headers = { "Content-Type": "application/octet-stream" };
     const chunks = Math.ceil(file.size / chunkSize) - 1;
@@ -72,6 +68,19 @@ const SinglePropertyDocumentsUpload = () => {
     if (isLastChunk) {
       // console.log(currentImageFileIndex === savedImageFiles.length - 1);
       setUinqueId(uuid());
+      await axios
+        .post(`/sam/v1/property/auth/upload-images`, detailsToPost, {
+          headers: authHeader,
+        })
+        .then((res) => {
+          if (res.data.msg !== 0) {
+            toast.error("Error while uploading files");
+          } else {
+            if (currentImageFileIndex === savedImageFiles.length - 1) {
+              toast.success("Files uploaded successfully");
+            }
+          }
+        });
       setLastUploadedImageFileIndex(currentImageFileIndex);
       setCurrentChunkIndexOfImage(null);
     } else {
