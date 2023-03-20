39bb256d89675bf266d9b748ca637b5c6d9fa5cd
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-03-20 13:13:52 +0530

    ok

--- src/Admin/Property/SinglePropertyDocumentsUpload.js
+++ src/Admin/Property/SinglePropertyDocumentsUpload.js
@@ -13,9 +13,28 @@ const SinglePropertyDocumentsUpload = () => {
           <AdminSideBar />
           <div className="col-xl-10 col-lg-9 col-md-8 mt-4 mt-md-0">
             <BreadCrumb />
-            <section className="upload-documents-wrapper wrapper">
+            <section className="upload-documents-wrapper">
               <div className="container-fluid">
-                <div className="row justify-content-center">{id}</div>
+                <h4 className="fw-bold text-primary">Property Id: {id}</h4>
+                <hr />
+                <div className="row border p-4">
+                  <h5 className="mb-3">Upload Property Documents</h5>
+                  <div className="col-4">
+                    <input type="file" className="form-control" />
+                  </div>
+                  <div className="col-6">
+                    <button className="btn btn-primary">Upload</button>
+                  </div>
+                </div>
+                <div className="row border p-4 mt-4">
+                  <h5 className="mb-3">Upload Property Images</h5>
+                  <div className="col-4">
+                    <input type="file" className="form-control" />
+                  </div>
+                  <div className="col-6">
+                    <button className="btn btn-primary">Upload</button>
+                  </div>
+                </div>
               </div>
             </section>
           </div>
