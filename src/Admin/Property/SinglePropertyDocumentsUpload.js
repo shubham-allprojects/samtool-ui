ad2c3cc9f3f322ead23ba9ab73dcc83c0e2a66af
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-03-20 15:20:35 +0530

    ok

--- src/Admin/Property/SinglePropertyDocumentsUpload.js
+++ src/Admin/Property/SinglePropertyDocumentsUpload.js
@@ -15,9 +15,22 @@ const SinglePropertyDocumentsUpload = () => {
             <BreadCrumb />
             <section className="upload-documents-wrapper">
               <div className="container-fluid">
-                <h4 className="fw-bold text-primary">Property Id: {id}</h4>
-                <hr />
+                <div className="row">
+                  <h4 className="fw-bold text-primary p-0">
+                    Property Id: {id}
+                  </h4>
+                  <hr />
+                </div>
                 <div className="row border p-4">
+                  <h5 className="mb-3">Upload Property Images</h5>
+                  <div className="col-4">
+                    <input type="file" multiple className="form-control" />
+                  </div>
+                  <div className="col-6">
+                    <button className="btn btn-primary">Upload</button>
+                  </div>
+                </div>
+                {/* <div className="row border p-4 mt-4">
                   <h5 className="mb-3">Upload Property Documents</h5>
                   <div className="col-4">
                     <input type="file" className="form-control" />
@@ -25,16 +38,7 @@ const SinglePropertyDocumentsUpload = () => {
                   <div className="col-6">
                     <button className="btn btn-primary">Upload</button>
                   </div>
-                </div>
-                <div className="row border p-4 mt-4">
-                  <h5 className="mb-3">Upload Property Images</h5>
-                  <div className="col-4">
-                    <input type="file" className="form-control" />
-                  </div>
-                  <div className="col-6">
-                    <button className="btn btn-primary">Upload</button>
-                  </div>
-                </div>
+                </div> */}
               </div>
             </section>
           </div>
