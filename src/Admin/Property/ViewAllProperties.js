039b679019a17e847b5c76fdbb3ed2c1f47218d0
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-03-23 15:51:35 +0530

    ok

--- src/Admin/Property/ViewAllProperties.js
+++ src/Admin/Property/ViewAllProperties.js
@@ -24,6 +24,7 @@ const ViewAllProperties = () => {
       { headers: authHeader }
     );
     setProperties(propertiesRes.data);
+    console.log(propertiesRes.data);
     setTimeout(() => {
       setLoading(false);
     }, 400);
