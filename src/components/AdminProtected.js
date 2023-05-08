524bb448afa2b65cad7ec8d965d7446a330849da
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-05-08 11:22:31 +0530

    ok

--- src/components/AdminProtected.js
+++ src/components/AdminProtected.js
@@ -1,14 +1,25 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Only loggedIn user will have access to the components wrapped in this component.
 const AdminProtected = ({ children }) => {
   const goTo = useNavigate();
-  const checkIsAdmin = () => {
+  const checkIsAdmin = async () => {
     const data = JSON.parse(localStorage.getItem("data"));
     if (data) {
       if (data.roleId !== 1) {
         goTo("/access-denied");
+      } else {
+        try {
+          let res = await axios.get(`/sam/v1/user-registration/logout`, {
+            headers: { Authorization: data.logintoken },
+          });
+          if (res.data !== "Valid user") {
+            localStorage.removeItem("data");
+            goTo("/login");
+          }
+        } catch (error) {}
       }
     } else {
       goTo("/access-denied");
