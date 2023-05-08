dcafc02de5eb440e0c575c6ce12961626253a135
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-05-08 11:07:26 +0530

    ok

--- src/components/ProtectedForLoggedInUser.js
+++ src/components/ProtectedForLoggedInUser.js
@@ -1,13 +1,24 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Only loggedIn user will have access to the components wrapped in this component.
 const ProtectedForLoggedInUser = ({ children }) => {
   const goTo = useNavigate();
-  const checkStatusOfLogin = () => {
+  const checkStatusOfLogin = async () => {
     const data = JSON.parse(localStorage.getItem("data"));
     if (!data) {
       goTo("/access-denied");
+    } else {
+      try {
+        let res = await axios.get(`/sam/v1/user-registration/logout`, {
+          headers: { Authorization: data.logintoken },
+        });
+        if (res.data !== "Valid user") {
+          localStorage.removeItem("data");
+          goTo("/login");
+        }
+      } catch (error) {}
     }
   };
   useEffect(() => {
