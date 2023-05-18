466c11bb179b1f13d8982364a18cb5234c7ad00d
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-05-18 12:58:18 +0530

    ok

--- src/CommonFunctions.js
+++ src/CommonFunctions.js
@@ -1,20 +1,3 @@
-export function counter(id, start, end, duration) {
-  let obj = document.getElementById(id),
-    current = start,
-    range = end - start,
-    increment = end > start ? 1 : -1,
-    step = Math.abs(Math.floor(duration / range));
-  if (obj) {
-    let timer = setInterval(() => {
-      current += increment;
-      obj.textContent = current;
-      if (current === end) {
-        clearInterval(timer);
-      }
-    }, step);
-  }
-}
-
 export const toggleClassOfNextPrevPageItems = () => {
   let activePageItem = document.querySelector(".page-item.active");
   if (activePageItem) {
