49f12a231b1b66789bf9652dc86bcb975ff9a880
Shubham Parab <shubham@smartrr.com>
2023-06-20 14:10:45 +0530

    ok

--- src/components/2.HomePage/Home.js
+++ src/components/2.HomePage/Home.js
@@ -665,6 +665,8 @@ function Home() {
                                 View
                               </button>
                               <button
+                                data-bs-toggle="modal"
+                                data-bs-target="#commentModal"
                                 className="btn btn-primary common-btn-font"
                                 style={{ width: "30%" }}
                               >
@@ -716,6 +718,54 @@ function Home() {
           </div>
         </div>
       </section>
+
+      {/* comment modal */}
+
+      <div
+        class="modal fade"
+        id="commentModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            {/* <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div> */}
+            <div class="modal-body">
+              <textarea
+                placeholder="some placeholder here"
+                name=""
+                id=""
+                rows="5"
+                className="form-control"
+                style={{ resize: "none" }}
+              ></textarea>
+              <div class="mt-2">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
